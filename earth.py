4b093289d30ec6a98ee75671108b1841e79833a0
Philip Han <46556419+Philipnah@users.noreply.github.com>
2021-02-06 15:05:30 +0100

    expanded server and client

--- earth.py
+++ earth.py
@@ -1,14 +1,17 @@
 import socket
 
-
-message = "Contact!"
+headerSize = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 8000))
+s.bind(("127.0.0.1", 8000))
 s.listen(5)
 
 while True:
      clientsocket, address = s.accept()
      print(f"Connection from {address} has been established.")
+     
+     message = "Contact!"
+     message = f'{len(message):<{headerSize}}' + message
      clientsocket.send(message.encode("utf-8"))
-     clientsocket.close()
+     
+     
--- mars.py
+++ mars.py
@@ -1,12 +1,38 @@
 import socket
-
 # socket.gethostname() = 192.168.56.1
 
+
+headerSize = 10
+
+message = "Contact!"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.56.1", 8000))
+s.connect(("127.0.0.1", 8000))
+
+
+
 
 while True:
-     buffersize = 256
-     message = s.recv(buffersize)
-     print(message.decode("utf-8"))
-     
+     fullMsg = ""
+     newMsg = True
+
+     while True:
+          
+          buffersize = 256
+          message = s.recv(buffersize)
+
+          if newMsg:
+               print(f"new message length: {message[:headerSize]}")
+               msgLength = int(message[:headerSize])
+               newMsg = False
+
+          fullMsg += message.decode("utf-8")
+          
+          if len(fullMsg) - headerSize == msgLength:
+               print("Full message received.\n")
+               print("Message:\n\n" + fullMsg[headerSize:])
+               newMsg = True
+               fullMsg = ""
+
+     print(fullMsg)
+          
